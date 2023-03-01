bdefc62f93a3904668e00be036b4937d6b6f8d25
Mary Rohrdanz <marohrdanz@mdanderson.org>
2023-03-01 09:48:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Extract version number from file
+
+This **extremely fragile** action extracts the version number from a file in a repo.
+This action was created specifically to extract the version number from 
+the [NG=CHM Viewer repo](https://github.com/MD-Anderson-Bioinformatics/NG-CHM).
+
+## Inputs
+
+- `file_path`: Path in repo to file containing version number
+- `start_string`: Starting characters of line containing version number
+
+## Outputs
+
+- `version_number`: Version number found in file\_path
+
+## Example usage
+
+In order to use this action, the repo must have already been checked out
+(e.g. via [GitHub Checkout Action](https://github.com/marketplace/actions/checkout).
+
+```yaml
+uses: marohrdanz/extract-version-from-file
+with:
+  file_path: 'NG-CHM/NGCHM/WebContent/javascript/CompatibilityManager.js'
+  start_string: 'CM.version = '
+```
+
---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const readline = require('readline');
+
+/**
+ * This is a quick-and-dirty script does the following:
+ *   - reads the file specified by input 'file_path' (reads line by line)
+ *   - finds the line that starts with the input 'start_string'
+ *   - extracts the version number from that line
+ *
+ * This was created specifically to get the version number from the 
+ * CompatibilityManager.js file in the NG-CHM project
+ * (https://github.com/MD-Anderson-Bioinformatics/NG-CHM)
+ */
+const filepath = core.getInput('file_path');
+core.debug(`Searching file: ${filepath}`);
+const filename = `${process.env.GITHUB_WORKSPACE}/${filepath}`
+
+const startString = core.getInput('start_string');
+core.debug(`Searching for string starting with: ${startString}`);
+
+const rl = readline.createInterface({
+  input: fs.createReadStream(filename),
+  crlfDelay: Infinity,
+});
+
+rl.on('line', (line) => {
+  if (line.startsWith(startString)) {
+    core.debug(`found line starting with start_string: ${line}`);
+    let version_number = line.replace(/^[^0-9]+|[^0-9]+$/g, "")
+    core.notice(`Found version number: ${version_number}`);
+    core.setOutput("version_number", version_number);
+  }
+});
+
+
