marohrdanz/extract-version-from-file

commit ad2bac4479ad9e7da54764ccdd2e62d701c5e543

Search for string anywhere in line
The original version of the code captured lines starting with the input
string. However, a proposed change to the NG-CHM viewer code adds
leading whitespace for indentation to the line containing the version
number.

I tried simply changing the search string to be '  CM.version ='.
However, github actions automatically strip leading whitespace. So I
made this action search for the string in the line (e.g. `.includes()`)
rather than lines that start with the input string (e.g.
`.startsWith()`).

This change hopefully makes this action a tiny bit less brittle.

index.js
@@ -6,10 +6,10 @@ const readline = require('readline');
 /**
  * This is a quick-and-dirty script does the following:
  *   - reads the file specified by input 'file_path' (reads line by line)
- *   - finds the line that starts with the input 'start_string'
+ *   - finds the line that contains the input 'search_string'
  *   - extracts the version number from that line
  *
- * This was created specifically to get the version number from the 
+ * This was created specifically to get the version number from the
  * CompatibilityManager.js file in the NG-CHM project
  * (https://github.com/MD-Anderson-Bioinformatics/NG-CHM)
  */
@@ -17,23 +17,26 @@ const filepath = core.getInput('file_path');
 core.debug(`Searching for version number in file: ${filepath}`);
 const filename = `${process.env.GITHUB_WORKSPACE}/${filepath}`
 
-const startString = core.getInput('start_string');
-core.debug(`Searching for string starting with: ${startString}`);
+const searchString = core.getInput('start_string');
+core.debug(`Searching for line containing: ${searchString}`);
 
 const rl = readline.createInterface({
   input: fs.createReadStream(filename),
   crlfDelay: Infinity,
 });
 
+let found = false;
 rl.on('line', (line) => {
-  if (line.startsWith(startString)) {
-    core.debug(`found line starting with start_string: ${line}`);
+  if (line.includes(searchString)) {
+    core.debug(`found line containing search_string: ${line}`);
     let version_number = line.replace(/^[^0-9]+|[^0-9]+$/g, "")
     core.notice(`Found version number '${version_number}' in file: ${filepath}`);
     core.setOutput("version_number", version_number);
-    return;
+    found = true;
   }
-});
-
-core.warning(`No line found starting with start_string: ${startString}`);
+}).on('close', () => {
+  if (!found) {
+    core.warning(`No line found containing search_string: ${searchString}`);
+  }
+})
 

action.yml
@@ -4,8 +4,8 @@ inputs:
   file_path:
     description: 'Path to file containing version number'
     required: true
-  start_string:
-    description: 'String for start of line containing version number'
+  search_string:
+    description: 'Search string for line containing version number'
     required: true
 outputs:
   version_number:

commit f60a6295bb4909902bce48ae4698c907fe3a20dc

Don't search comment lines
What a brittle mess. Testing after ad2bac4 caused things to fail because
there are comment lines in CompatiblityManager.js that include the
search string "CM.version = ". Soooooo, hacking things up a bit more
(this entire action is a hack really).

index.js
@@ -27,7 +27,7 @@ const rl = readline.createInterface({
 
 let found = false;
 rl.on('line', (line) => {
-  if (line.includes(searchString)) {
+  if (line.includes(searchString) && !line.startsWith("  //")) {
     core.debug(`found line containing search_string: ${line}`);
     let version_number = line.replace(/^[^0-9]+|[^0-9]+$/g, "")
     core.notice(`Found version number '${version_number}' in file: ${filepath}`);
@@ -37,6 +37,8 @@ rl.on('line', (line) => {
 }).on('close', () => {
   if (!found) {
     core.warning(`No line found containing search_string: ${searchString}`);
+  } else {
+    core.debug(`Version number found in file: ${filepath}`);
   }
 })
 